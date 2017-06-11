2e5cec223dcb465ba38f9b75e4193f05111c6348
edo1z <edo1z3145@gmail.com>
2017-06-12 04:04:40 +0900

    func

--- go_study.go
+++ go_study.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"math/rand"
 )
 
 func main() {
-	num := rand.Intn(10)
-	pi := math.Pi
-	fmt.Printf("Number is %d\n", num)
-	fmt.Printf("PI is %f\n", pi)
+	fmt.Println(add(12, 15))
+}
+
+func add(x, y int) int {
+	return x + y
 }
