51a3f69757dbee5254bdf89e02c22775cca2aaa3
edo1z <edo1z3145@gmail.com>
2017-06-12 04:24:55 +0900

    Named return values

--- go_study.go
+++ go_study.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 )
 
-func swap(x, y string) (string, string) {
-	return y, x
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	//naked return
+	return
 }
 
 func main() {
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
+	fmt.Println("Named return values")
+	fmt.Println(split(10))
 }
