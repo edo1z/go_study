9a56a0ced15d897c2627e5907dae8ccaf6dd9ce9
edo1z <edo1z3145@gmail.com>
2017-06-12 04:00:59 +0900

    package

--- go_study.go
+++ go_study.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
 )
 
 func main() {
-	fmt.Println("Hello world.")
+	num := rand.Intn(10)
+	pi := math.Pi
+	fmt.Printf("Number is %d\n", num)
+	fmt.Printf("PI is %f\n", pi)
 }
