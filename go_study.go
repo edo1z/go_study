6af37541d1ccb622fb0444c09c0be44098303778
edo1z <edo1z3145@gmail.com>
2017-06-12 18:19:58 +0900

    Map literals

--- go_study.go
+++ go_study.go
@@ -8,12 +8,17 @@ type Vertex struct {
 	Lat, Long float64
 }
 
-var m map[string]Vertex
+var m = map[string]Vertex{
+	"Bell Labs": Vertex{
+		123.123, -234.234,
+	},
+	"Google": Vertex{
+		555.555, -122.122,
+	},
+}
 
 func main() {
-	m = make(map[string]Vertex)
-	m["Bell Labs"] = Vertex{
-		40.68443, -74.39967,
+	for i, v := range m {
+		fmt.Printf("%s => %f\n", i, v)
 	}
-	fmt.Println(m["Bell Labs"])
 }
