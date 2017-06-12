cffbf5036ccd7443f5358a09fd675f08c1a306a5
edo1z <edo1z3145@gmail.com>
2017-06-12 20:07:16 +0900

    Exercise: Fibonacci closure

--- go_study.go
+++ go_study.go
@@ -1,23 +1,19 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func adder() func(int) int {
-	sum := 0
-	return func(x int) int {
-		sum += x
-		return sum
+func fibonacci() func() int {
+	a, b := 0, 1
+	return func() int {
+		result := a
+		a, b = b, a+b
+		return result
 	}
 }
 
 func main() {
-	pos, neg := adder(), adder()
+	f := fibonacci()
 	for i := 0; i < 10; i++ {
-		fmt.Println(
-			pos(i),
-			neg(-2*i),
-		)
+		fmt.Println(f())
 	}
 }
