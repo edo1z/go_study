0aaf3d03f87d6cd71d71329f5a33d1851dd2611e
edo1z <edo1z3145@gmail.com>
2017-06-12 16:37:37 +0900

    Slice length and capacity

--- go_study.go
+++ go_study.go
@@ -6,13 +6,20 @@ import (
 
 func main() {
 	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
 
-	s = s[1:4]
-	fmt.Println(s)
+	s = s[1:2]
+	printSlice(s)
 
-	s = s[:2]
-	fmt.Println(s)
+	s = s[3:4]
+	printSlice(s)
 
 	s = s[1:]
-	fmt.Println(s)
+	printSlice(s)
+
+	fmt.Println("?")
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
