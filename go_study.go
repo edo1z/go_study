800c7a28417df0e30938a0c90d5082cf9b3b2c04
edo1z <edo1z3145@gmail.com>
2017-06-12 13:32:58 +0900

    Zero values

--- go_study.go
+++ go_study.go
@@ -2,18 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math/cmplx"
-)
-
-var (
-	ToBe   bool       = false
-	MaxInt uint64     = 1<<64 - 1
-	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 func main() {
-	const f = "%T(%v)\n"
-	fmt.Printf(f, ToBe, ToBe)
-	fmt.Printf(f, MaxInt, MaxInt)
-	fmt.Printf(f, z, z)
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
 }
