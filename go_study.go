994a36e580d714b483ffe6ff7e5bec22ab1bf17a
edo1z <edo1z3145@gmail.com>
2017-06-12 18:13:26 +0900

    Maps

--- go_study.go
+++ go_study.go
@@ -1,20 +1,19 @@
 package main
 
-import "golang.org/x/tour/pic"
+import (
+	"fmt"
+)
 
-func Pic(dx, dy int) [][]uint8 {
-	image := make([][]uint8, dy)
-	for y := range image {
-		image[y] = make([]uint8, dx)
-	}
-	for y := 0; y < dy; y++ {
-		for x := 0; x < dx; x++ {
-			image[y][x] = uint8((x + y) / 2)
-		}
-	}
-	return image
+type Vertex struct {
+	Lat, Long float64
 }
 
+var m map[string]Vertex
+
 func main() {
-	pic.Show(Pic)
+	m = make(map[string]Vertex)
+	m["Bell Labs"] = Vertex{
+		40.68443, -74.39967,
+	}
+	fmt.Println(m["Bell Labs"])
 }
