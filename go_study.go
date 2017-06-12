6cf50c9ffe17f207cae626bbb9f7fcf75b957f14
edo1z <edo1z3145@gmail.com>
2017-06-12 16:40:01 +0900

    Nil slices

--- go_study.go
+++ go_study.go
@@ -5,21 +5,9 @@ import (
 )
 
 func main() {
-	s := []int{2, 3, 5, 7, 11, 13}
-	printSlice(s)
-
-	s = s[1:2]
-	printSlice(s)
-
-	s = s[3:4]
-	printSlice(s)
-
-	s = s[1:]
-	printSlice(s)
-
-	fmt.Println("?")
-}
-
-func printSlice(s []int) {
-	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+	var s []int
+	fmt.Println(s, len(s), cap(s))
+	if s == nil {
+		fmt.Println("nil!")
+	}
 }
