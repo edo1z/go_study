43cb7892491dc16eb2617b261874b7815cd2abee
edo1z <edo1z3145@gmail.com>
2017-06-12 13:25:40 +0900

    short variable declarations

--- go_study.go
+++ go_study.go
@@ -4,14 +4,10 @@ import (
 	"fmt"
 )
 
-var c, python, php bool
-
-func hoge() {
-	c = true
-}
-
 func main() {
-	var i int
-	hoge()
-	fmt.Println(i, c, python, php)
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
 }
