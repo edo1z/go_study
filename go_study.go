b906975acc31cdd09762dfa5c1ae101fe2700340
edo1z <edo1z3145@gmail.com>
2017-06-12 15:21:10 +0900

    Arrays

--- go_study.go
+++ go_study.go
@@ -8,13 +8,17 @@ type Vertex struct {
 	X, Y int
 }
 
-var (
-	v1 = Vertex{1, 2}
-	v2 = Vertex{X: 1}
-	v3 = Vertex{}
-	p  = &Vertex{1, 2}
-)
-
 func main() {
-	fmt.Println(v1, p, v2, v3)
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	primes[3] = 9
+	fmt.Println(primes)
+
+	fmt.Println(len(a))
+	fmt.Println(len(primes))
 }
