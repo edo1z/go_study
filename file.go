676c62d32737c715b41d0a8aa8c0f35f6e77c5df
edo1z <edo1z3145@gmail.com>
2017-06-14 17:46:27 +0900

    several patarns of read file

--- file.go
+++ file.go
@@ -1,13 +1,73 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 )
 
-func getFileName() string {
+//コマンドライン引数からファイルパスを取得
+func GetFileName() string {
 	if len(os.Args) < 2 {
 		log.Fatal("Please input file name")
 	}
 	return os.Args[1]
+}
+
+//ファイル全体の文字列を返す
+func ReadAllOfFile(file_path string) string {
+	bs, err := ioutil.ReadFile(file_path)
+	ChkErr(err)
+	return string(bs)
+}
+
+//ファイル全体の文字列を返す
+func ReadAllOfFile2(file_path string) string {
+	fp, err := os.Open(file_path)
+	ChkErr(err)
+	bs, err := ioutil.ReadAll(fp)
+	ChkErr(err)
+	return string(bs)
+}
+
+func Readln(r *bufio.Reader) (string, error) {
+	var (
+		isPrefix bool  = true
+		err      error = nil
+		line, ln []byte
+	)
+	for isPrefix && err == nil {
+		line, isPrefix, err = r.ReadLine()
+		ln = append(ln, line...)
+	}
+	return string(ln), err
+}
+
+//ファイル全体を表示
+func ReadFile(file_path string) {
+	fp, err := os.Open(file_path)
+	ChkErr(err)
+	//defer fp.Close()
+	reader := bufio.NewReader(fp)
+	str, err := Readln(reader)
+	for err == nil {
+		fmt.Println(str)
+		str, err = Readln(reader)
+	}
+}
+
+func WriteFile() string {
+	return "string"
+}
+
+//func OpenOrCreateFile() *os.File {
+
+//}
+
+func ChkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go_study.go
+++ go_study.go
@@ -1,41 +1,18 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
 )
 
-func Readln(r *bufio.Reader) (string, error) {
-	var (
-		isPrefix bool  = true
-		err      error = nil
-		line, ln []byte
-	)
-	for isPrefix && err == nil {
-		line, isPrefix, err = r.ReadLine()
-		ln = append(ln, line...)
-	}
-	return string(ln), err
-}
-
-func newFile(fn string) *os.File {
-	fp, err := os.OpenFile(fn, os.O_RDONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return fp
-}
-
 func main() {
-	fp := newFile("./data/data.txt")
-	defer fp.Close()
-	reader := bufio.NewReader(fp)
+	fmt.Println("hello world")
 
-	str, err := Readln(reader)
-	for err == nil {
-		fmt.Println(str)
-		str, err = Readln(reader)
-	}
+	file_path := GetFileName()
+	content := ReadAllOfFile(file_path)
+	fmt.Println(content)
+
+	content = ReadAllOfFile2(file_path)
+	fmt.Println(content)
+
+	ReadFile(file_path)
 }
