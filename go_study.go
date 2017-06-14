e33c886351ab9cb30211945bc8d3517d2f37e99e
edo1z <edo1z3145@gmail.com>
2017-06-15 01:11:21 +0900

    Goroutines

--- go_study.go
+++ go_study.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
 func main() {
-	fmt.Println("hello world")
-
-	file_path := GetFileName()
-	content := ReadAllOfFile(file_path)
-	fmt.Println(content)
-
-	content = ReadAllOfFile2(file_path)
-	fmt.Println(content)
-
-	ReadFile(file_path)
+	go say("world")
+	say("hello")
 }
