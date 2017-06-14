4df9dc8e12ee286671de34a0188c6da38eae37f2
edo1z <edo1z3145@gmail.com>
2017-06-14 16:53:46 +0900

    read file

--- go_study.go
+++ go_study.go
@@ -1,16 +1,41 @@
 package main
 
 import (
-	_ "bufio"
-	_ "fmt"
+	"bufio"
+	"fmt"
 	"log"
 	"os"
 )
 
-func main() {
-	f, err := os.Open("data.txt")
+func Readln(r *bufio.Reader) (string, error) {
+	var (
+		isPrefix bool  = true
+		err      error = nil
+		line, ln []byte
+	)
+	for isPrefix && err == nil {
+		line, isPrefix, err = r.ReadLine()
+		ln = append(ln, line...)
+	}
+	return string(ln), err
+}
+
+func newFile(fn string) *os.File {
+	fp, err := os.OpenFile(fn, os.O_RDONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
+	return fp
+}
+
+func main() {
+	fp := newFile("./data/data.txt")
+	defer fp.Close()
+	reader := bufio.NewReader(fp)
+
+	str, err := Readln(reader)
+	for err == nil {
+		fmt.Println(str)
+		str, err = Readln(reader)
+	}
 }
