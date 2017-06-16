885c209b45d0a92d6d35b4aed1d20f89bd274a54
edo1z <edo1z3145@gmail.com>
2017-06-16 18:20:22 +0900

    ebiten hello world

--- go_study.go
+++ go_study.go
@@ -1,35 +1,15 @@
 package main
 
 import (
-	"github.com/nfnt/resize"
-	"image/jpeg"
-	"io"
-	"log"
-	"os"
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
+func update(screen *ebiten.Image) error {
+	_ = ebitenutil.DebugPrint(screen, "Hello World")
+	return nil
+}
+
 func main() {
-	f, err := os.Open("./data/hoge.jpg")
-	chkErr(err)
-	img, err := jpeg.Decode(f)
-	chkErr(err)
-	_ = f.Close()
-
-	thumb := resize.Thumbnail(300, 300, img, resize.Lanczos3)
-
-	out, err := os.Create("./data/thumb.jpg")
-	chkErr(err)
-	defer Close(out)
-	err = jpeg.Encode(out, thumb, nil)
-	chkErr(err)
-}
-
-func Close(c io.Closer) {
-	_ = c.Close()
-}
-
-func chkErr(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
+	_ = ebiten.Run(update, 320, 240, 2, "Hello")
 }
