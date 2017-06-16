bdc8509d6a45d98e02efda5dfe5ccf0d0de0fbd0
edo1z <edo1z3145@gmail.com>
2017-06-16 21:02:21 +0900

    gocron hello world

--- go_study.go
+++ go_study.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"fmt"
+	"github.com/jasonlvhit/gocron"
 )
 
-func update(screen *ebiten.Image) error {
-	_ = ebitenutil.DebugPrint(screen, "Hello World")
-	return nil
+func task(a, b int) {
+	fmt.Println("I am running task.")
+	fmt.Printf("%d + %d = %d", a, b, a+b)
 }
 
 func main() {
-	_ = ebiten.Run(update, 320, 240, 2, "Hello")
+	gocron.Clear()
+	s := gocron.NewScheduler()
+	s.Every(1).Minute().Do(task, 10, 5)
+	s.Start()
 }
