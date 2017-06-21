9f1d6b04b708d4f56a739dbb4cb719e5fea42655
edo1z <edo1z3145@gmail.com>
2017-06-21 23:08:44 +0900

    test web server

--- main.go
+++ main.go
@@ -3,51 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
-	"github.com/syndtr/goleveldb/leveldb"
+	"net/http"
+	"strings"
 )
 
 func main() {
-	db := db_init()
-	defer func(){
-		fmt.Println("Closing DB...")
-		db.Close()
-	}()
-
-	val, err := db.Get([]byte("key"), nil)
+	http.HandleFunc("/", sayhello)
+	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
-		fmt.Println("Key is not exist")
-		put(db, []byte("key"), []byte("hello"))
-		val, err = db.Get([]byte("key"), nil)
-		if err != nil {
-			log.Fatal("get error")
-		}
-	}
-	fmt.Printf("key is %s\n", string(val))
-
-	del(db, []byte("key"))
-}
-
-func db_init() *leveldb.DB {
-	fmt.Println("open database")
-	db, err := leveldb.OpenFile("./data/db", nil)
-	if err != nil {
-		log.Fatal("open error")
-	}
-	return db
-}
-
-func put(db *leveldb.DB, key []byte, value []byte) {
-	fmt.Println("put to db")
-	err := db.Put(key, value, nil)
-	if err != nil {
-		log.Fatal("put error")
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
-func del(db *leveldb.DB, key []byte) {
-	fmt.Println("del from db")
-	err := db.Delete(key, nil)
+func sayhello(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
 	if err != nil {
-		log.Fatal("delete error")
+		log.Fatal(err)
 	}
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello hogehoge!")
 }
