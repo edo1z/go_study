e1eeeb35436e43b40f4dfe551f9893da27a37c49
edo1z <edo1z3145@gmail.com>
2017-06-28 23:02:06 +0900

    iris + gorilla/sessionでセッション管理テスト

--- main.go
+++ main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
-	"gopkg.in/gomail.v2"
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/context"
+	"github.com/gorilla/sessions"
+	"fmt"
 )
 
-func main() {
-	m := gomail.NewMessage()
-	m.SetHeader("From", "from@example.com")
-	m.SetHeader("To", "to@example.com")
-	m.SetHeader("Subject", "日本語")
-	m.SetBody("text/plain", "日本語でこんにちは")
+var store = sessions.NewCookieStore([]byte("hoge"))
+var sess_name = "sess"
 
-	d := gomail.Dialer{Host: "localhost", Port: 25}
-	if err := d.DialAndSend(m); err != nil {
-		panic(err)
-	}
+func main() {
+	app := iris.New()
+	app.Get("/", topPage)
+	app.Get("up", upPage)
+	app.Get("cl", clearPage)
+	app.Run(iris.Addr(":8080"))
+}
+
+func topPage (ctx context.Context) {
+	f := getFlash(ctx)
+	cnt := fmt.Sprintf(
+		"<h2>count is %d</h2>",
+		getCount(ctx),
+	)
+	html := `<br><a href="/up">Count Up</a>
+	<br><a href="/cl">Count Clear</a>`
+	ctx.HTML(f + cnt + html)
+}
+
+func upPage (ctx context.Context) {
+	countUp(ctx)
+	addFlash(ctx, "count up")
+	ctx.Redirect("/")
+}
+
+func clearPage (ctx context.Context) {
+	sessClear(ctx)
+	addFlash(ctx,"session clear")
+	ctx.Redirect("/")
 }
