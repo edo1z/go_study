76bcacc974ae0aa6bf42815b4aad2f22c42e024c
edo1z <edo1z3145@gmail.com>
2017-06-30 10:57:04 +0900

    filepath test

--- main.go
+++ main.go
@@ -1,40 +1,9 @@
 package main
 
 import (
-	"html/template"
-	"bytes"
-	"fmt"
+	"github.com/edo1z/go_study/hoge"
 )
 
-var viewPath = "../../views/"
-var contentType = "text/plain"
-
-type Contact struct {
-	Company	string
-	Name	string
-	Mail	string
-	Content	string
-}
-
 func main() {
-	hoge()
-	hoge()
+	hoge.Hoge("data.txt")
 }
-
-func hoge() {
-	c := Contact{
-		Company: "会社",
-		Name: "太郎",
-		Mail: "hoge@example.com",
-		Content: "どうしたらいいですか？",
-	}
-	t, err := template.ParseFiles("./mail.tpl", "./welcome.html")
-	if err != nil {
-		fmt.Println(err)
-	}else{
-		buff := &bytes.Buffer{}
-		t.Execute(buff, c)
-		fmt.Println(buff.String())
-	}
-}
-
