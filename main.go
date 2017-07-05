1500d8758ce7efcc0a95623b1ad10212a03bea3b
edo1z <edo1z3145@gmail.com>
2017-07-05 15:43:33 +0900

    echo - post

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"net/http"
 )
 
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", hello)
 	e.GET("/:id", helloId)
 	e.GET("/show", show)
+	e.POST("/users/add", newUser)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
@@ -30,4 +37,12 @@ func show(c echo.Context) error {
 		msg = "team: " + team + ", member: " + member
 	}
 	return c.String(http.StatusOK, msg)
+}
+
+func newUser(c echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, u)
 }
