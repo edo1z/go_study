8fe9a1201da0d7ef5f7c3eadd8a68fcd75074e85
edo1z <edo1z3145@gmail.com>
2017-07-05 15:21:05 +0900

    echo first

--- mail.tpl
+++
@@ -1,5 +0,0 @@
-{{template "head"}}
-
-{{.Name}}
-
-{{.Content}}
--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	"github.com/edo1z/go_study/hoge"
+	"github.com/labstack/echo"
+	"net/http"
 )
 
 func main() {
-	hoge.Hoge("data.txt")
+	e := echo.New()
+	e.GET("/", hello)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
